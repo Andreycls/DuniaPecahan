45722bad9de0db1e54c26e703abbd6eb8197410e
Andrey Cls <Andrey_Andrzz@rocketmail.com>
2018-04-27 08:47:29 +0700

    Add files via upload

---
+++ Petunjuk.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Petunjuk : MonoBehaviour {
+public Button BmulaiMain;
+public Button BPetunjuk;
+public Button BBack;
+	// Use this for initialization
+	// Update is called once per frame
+	    void Start()
+        {
+		//		Button BBack = gameObject.GetComponent<Button>();
+			//		BBack.onClick.AddListener(delegate() { StartGame("Dashboard"); });
+			    
+			
+            
+        }
+       
+        public void StartGame(string level)
+        {
+            Application.LoadLevel(level);
+        }
+
+	
+	public void BackOnClick()
+    {
+		//		BackButton.onClick.AddListener(delegate() { StartGame("Dashboard"); });	
+			
+        SceneManager.LoadScene("Dashboard");
+    }
+}
